f4e8b4123b1f38e908414cc2dcd764c59c0f41eb
ecarist <33216648+ecarist@users.noreply.github.com>
2021-03-16 16:15:53 +0800

    Update PromoServiceImpl.java

--- src/main/java/com/miaoshaproject/service/impl/PromoServiceImpl.java
+++ src/main/java/com/miaoshaproject/service/impl/PromoServiceImpl.java
@@ -2,62 +2,134 @@ package com.miaoshaproject.service.impl;
 
 import com.miaoshaproject.dao.PromoDOMapper;
 import com.miaoshaproject.dataobject.PromoDO;
+import com.miaoshaproject.error.BusinessException;
+import com.miaoshaproject.error.EmBusinessError;
+import com.miaoshaproject.service.ItemService;
 import com.miaoshaproject.service.PromoService;
+import com.miaoshaproject.service.UserService;
+import com.miaoshaproject.service.model.ItemModel;
 import com.miaoshaproject.service.model.PromoModel;
+import com.miaoshaproject.service.model.UserModel;
 import org.joda.time.DateTime;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
- * @author shkstart
- * @create 2021-02-26-22:21
+ * @author 
+
  */
 @Service
 public class PromoServiceImpl implements PromoService {
+
     @Autowired
     private PromoDOMapper promoDOMapper;
+
+    @Autowired
+    private ItemService itemService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Autowired
+    private UserService userService;
+
     @Override
     public PromoModel getPromoByItemId(Integer itemId) {
+        // 获取对应商品的秒杀活动信息
         PromoDO promoDO = promoDOMapper.selectByItemId(itemId);
-        //将Do转换成Model
-        PromoModel promoModel = convertFromPromoDo(promoDO);
-        //如果promoModel为null，没有该商品的秒杀活动
-        if(promoModel==null)return null;
-        //判断当前时间和秒杀开始时间的关系
-        //开始时间在当前时间之后
-        if(promoModel.getStartDate().isAfterNow()){
-            promoModel.setStatus(1);
-        }else if(promoModel.getEndDate().isBeforeNow()){
-            //秒杀已经结束
-            promoModel.setStatus(3);
-        }else{
-            //秒杀正在进行
-            promoModel.setStatus(2);
+        PromoModel promoModel = convertModelFromDataObject(promoDO);
+        if (promoModel == null) { // 当前商品没有秒杀活动
+            return null;
         }
-        //
-
+        // 判断当前时间是否秒杀活动即将开始或正在进行
+        DateTime now = new DateTime();
+        if (promoModel.getStartDate().isAfterNow()) { // 开始时间在当前时间之后，活动还未开始
+            promoModel.setStatus(1);
+        } else if (promoModel.getEndDate().isBeforeNow()) { // 结束时间在当前时间之前，活动已结束
+            promoModel.setStatus(3);
+        } else {
+            promoModel.setStatus(2); // 活动正在进行中
+        }
         return promoModel;
     }
 
-//  promoDo到promoModel转换
-    private PromoModel convertFromPromoDo(PromoDO promoDo){
+    @Override
+    public void publishPromo(Integer promoId) {
+        // 通过活动id获取活动
+        PromoDO promoDO = promoDOMapper.selectByPrimaryKey(promoId);
+        if (promoDO.getItemId() == null || promoDO.getItemId().intValue() == 0) {
+            return;
+        }
+        ItemModel itemModel = itemService.getItemById(promoDO.getItemId()); // 注意：活动开始前提前将库存放入缓存，如果此段时间有卖出，则会导致库存存在问题，因此这里设置活动开始前不能卖出
+        // 将库存同步到redis内
+        redisTemplate.opsForValue().set("promo_item_stock_" + itemModel.getId(), itemModel.getStock());
 
-        if(promoDo ==null){
+        // 将大闸的限制数字设到redis内，这里设置大闸限制数字为当前库存的5倍
+        redisTemplate.opsForValue().set("promo_door_count_" + promoId, itemModel.getStock().intValue() * 5);
+    }
+
+    @Override
+    public String generateSecondKillToken(Integer promoId, Integer itemId, Integer userId) {
+        // 判断库存是否已售罄，若对应的售罄key存在，则直接返回下单失败
+        if (redisTemplate.hasKey("promo_item_stock_invalid_" + itemId)) {
+            return null;
+        }
+        // 获取对应商品的秒杀活动信息
+        PromoDO promoDO = promoDOMapper.selectByPrimaryKey(promoId);
+        PromoModel promoModel = convertModelFromDataObject(promoDO);
+        if (promoModel == null) { // 当前商品没有秒杀活动
+            return null;
+        }
+        // 判断当前时间是否秒杀活动即将开始或正在进行
+        DateTime now = new DateTime();
+        if (promoModel.getStartDate().isAfterNow()) { // 开始时间在当前时间之后，活动还未开始
+            promoModel.setStatus(1);
+        } else if (promoModel.getEndDate().isBeforeNow()) { // 结束时间在当前时间之前，活动已结束
+            promoModel.setStatus(3);
+        } else {
+            promoModel.setStatus(2); // 活动正在进行中
+        }
+        // 判断活动是否正在进行
+        if (promoModel.getStatus().intValue() != 2) {
+            return null;
+        }
+        // 判断item信息是否存在
+        ItemModel itemModel = itemService.getItemByIdInCache(itemId);
+        if (itemModel == null) {
+            return null;
+        }
+        // 判断用户是否存在
+        UserModel userModel = userService.getUserByIdInCache(userId);
+        if (userModel == null) {
             return null;
         }
 
+        // 获取秒杀大闸的count数量
+        Long result = redisTemplate.opsForValue().increment("promo_door_count_" + promoId, -1);
+        if (result < 0) {
+            return null;
+        }
+
+        // 生成token并且存入redis内，并给一个5min的有效期
+        String token = UUID.randomUUID().toString().replace("-", "");
+        redisTemplate.opsForValue().set("promo_token_" + promoId + "_userid_" + userId + "_itemid_" + itemId, token);
+        redisTemplate.expire("promo_token_" + promoId + "_userid_" + userId + "_itemid_" + itemId, 5, TimeUnit.MINUTES);
+        return token;
+    }
+
+    private PromoModel convertModelFromDataObject(PromoDO promoDO) {
+        if (promoDO == null) {
+            return null;
+        }
         PromoModel promoModel = new PromoModel();
-        BeanUtils.copyProperties(promoDo,promoModel);
-        //单独这是价格的
-        promoModel.setPromoItemPrice(new BigDecimal(promoDo.getPromoItemPrice()));
-        //单独设置时间，mysql是sql.date,model是joda-date
-        promoModel.setStartDate(new DateTime(promoDo.getStartDate()));
-        promoModel.setEndDate(new DateTime(promoDo.getEndDate()));
-
+        BeanUtils.copyProperties(promoDO, promoModel);
+        promoModel.setStartDate(new DateTime(promoDO.getStartDate())); // 这里需要将Date和DateTime做一个转换
+        promoModel.setEndDate(new DateTime(promoDO.getEndDate()));
         return promoModel;
-
     }
 }
