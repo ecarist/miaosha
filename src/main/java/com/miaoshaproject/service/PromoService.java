5f37c207dd33777ebbec3578266210b04999f15e
ecarist <33216648+ecarist@users.noreply.github.com>
2021-03-16 16:12:10 +0800

    Update PromoService.java

--- src/main/java/com/miaoshaproject/service/PromoService.java
+++ src/main/java/com/miaoshaproject/service/PromoService.java
@@ -3,10 +3,15 @@ package com.miaoshaproject.service;
 import com.miaoshaproject.service.model.PromoModel;
 
 /**
- * @author shkstart
+ * @author 
  * @create 2021-02-26-22:20
  */
 public interface PromoService {
     //根据商品id获取该商品即将进行或正在进行的秒杀信息
     PromoModel getPromoByItemId(Integer itemId);
+    // 活动发布
+    void publishPromo(Integer promoId);
+
+    // 生成秒杀用的令牌
+    String generateSecondKillToken(Integer promoId, Integer itemId, Integer userId);
 }
