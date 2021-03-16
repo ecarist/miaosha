eb4d57ac5aa0ca3ce99965249a886c5008a4965d
ecarist <33216648+ecarist@users.noreply.github.com>
2021-03-16 16:11:33 +0800

    Update OrderService.java

--- src/main/java/com/miaoshaproject/service/OrderService.java
+++ src/main/java/com/miaoshaproject/service/OrderService.java
@@ -10,5 +10,6 @@ import com.miaoshaproject.service.model.OrderModel;
 //处理交易模型的service
 public interface OrderService {
     //下单方法：哪个用户，买哪个商品，买多少个
-    OrderModel createOrder(Integer userId,Integer itemId,Integer promoId,Integer amount) throws BusinessException;
+//     OrderModel createOrder(Integer userId,Integer itemId,Integer promoId,Integer amount) throws BusinessException;
+    OrderModel createOrder(Integer userId, Integer itemId, Integer promoId, Integer amount, String stockLogId) throws BusinessException;
 }
